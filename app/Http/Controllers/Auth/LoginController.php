b192a25af9e73e9258414bf88cca1ec39f015611
Ержан Шалдыбаев <erzhan.hype@gmail.com>
2021-09-30 17:22:26 +0600

    auth complete

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -23,46 +23,6 @@ class LoginController extends Controller
         $this->middleware('guest')->except('logout');
     }
 
-    protected function auth(Request $request)
-    {
-        $pem = $request->pem;
-        if($pem){
-            $b64 = base64_encode($pem);
-            $result = array();
-            $res = exec('cd /opt/ksign && LD_LIBRARY_PATH="/opt/kalkancrypt:/opt/kalkancrypt/lib/engines" php secure.php '.$b64, $result);
-
-            if($result[0] == ''){
-                $j = $result[1];
-            }else{
-                $j = $result[0];
-            }
-
-            $p = json_decode(base64_decode($j));
-            return response()->json(['data' => $p]);
-        }else{
-            return response()->json(['error' => 'Key is not readable!']); 
-        }
-    }
-
-    protected function sign(Request $request)
-    {
-        $hash = $request->hash;
-        $sign = $request->sign;
-
-        if($hash && $sign){
-            $check = array();
-            exec('cd /opt/ksign && LD_LIBRARY_PATH="/opt/kalkancrypt:/opt/kalkancrypt/lib/engines" php check.php '.$hash.' '.$sign, $check);
-            $j = json_decode(base64_decode($check[0]));
-
-            if($j->status == 'SUCCESS') {
-                return response()->json(['data' => $j]);
-            }else{
-                return response()->json(['error' => 'Подпись не прошла проверку!']);  
-            }
-        }else{
-            return response()->json(['error' => 'Hash or Sign is not requested!']); 
-        }
-    }
 
     /**
      * Attempt to log the user into the application.
